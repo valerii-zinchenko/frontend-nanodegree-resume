71439be0df248714e1c8fdf5fc45a0239bf550b4
valerii-zinchenko <zinchenko.valerii@gmail.com>
2016-05-08 21:21:42 +0300

    move inName function from resumeBuilder.js to helper.js for safety

--- js/helper.js
+++ js/helper.js
@@ -68,6 +68,22 @@ var googleMap = '<div id="map"></div>';
 /*
 The International Name challenge in Lesson 2 where you'll create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
 */
+function inName(name) {
+	name = name.trim().split(" ");
+
+	if (name && name.length === 0) {
+		return name;
+	}
+
+	name[0] = name[0][0].toUpperCase() + name[0].slice(1).toLowerCase();
+
+	if (name[1]) {
+		name[1] = name[1].toUpperCase();
+	}
+
+	return name.join(" ");
+}
+
 $(document).ready(function() {
   $('button').click(function() {
     var iName = inName ? inName(data.bio.name) : (function(){ return ""; })();
